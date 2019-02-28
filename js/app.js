11ff53b8d57f5b91e0f1d05398ce51948353d5d6
ilavisharma <lavisharma796@gmail.com>
2019-02-28 10:26:38 +0530

    fill the new form with some fake data

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 var assetCount = 0;
 $(document).ready(() => {
   renderPageContent();
-  console.clear();
 });
 function renderPageContent() {
   AssetTrackerContract.methods.getAssetCount().call((error, response) => {
     if (error) console.log(error);
     else {
-      assetCount = response[0];
-      $("#count").html("Total " + response[0] + " Assets in the Blockchain");
+      assetCount = response;
+      $("#count").html("Total " + response + " Assets");
       renderTable();
     }
   });
@@ -47,14 +46,6 @@ function renderPageContent() {
 }
 
 function createNewAsset() {
-  // // generate data
-  // // $('input[name="batchNo"]').val(faker.random.number());
-  // document.querySelector('input[name="batchNo"]').value = faker.random.number();
-  // $('input[name="name"]').val(faker.commerce.product());
-  // $('input[name="desc"]').val(faker.lorem.sentence());
-  // $('input[name="manufacturer"]').val(faker.company.companyName());
-  // $('input[name="owner"]').val(faker.company.companyName());
-
   let batchNo = $('input[name="batchNo"]').val();
   let name = $('input[name="name"]').val();
   let desc = $('input[name="desc"]').val();
@@ -67,12 +58,39 @@ function createNewAsset() {
     .createAsset(batchNo, name, desc, manufacturer, owner, status)
     .send()
     .then(result => {
-      console.log(result);
       if (result.status === true) {
         alert("Success");
+        console.log(result);
+        $("#loading").show();
         $("tbody").html("");
         renderPageContent();
+        // clear the form
+        $('input[name="batchNo"]').val("");
+        $('input[name="name"]').val("");
+        $('input[name="desc"]').val("");
+        $('input[name="manufacturer"]').val("");
+        $('input[name="owner"]').val("");
+        $('input[name="status"]').val("");
       }
     });
   $("#exampleModal").modal("hide");
 }
+
+function generateAsset() {
+  return {
+    batchNo: faker.random.number().toString(),
+    name: faker.commerce.product(),
+    desc: faker.lorem.sentence(),
+    manufacturer: faker.company.companyName(),
+    owner: faker.company.companyName()
+  };
+}
+
+$("#exampleModal").on("shown.bs.modal", e => {
+  // fill the modal form with fake data when modal is shown
+  $('input[name="batchNo"]').val(faker.random.number().toString());
+  $('input[name="name"]').val(faker.commerce.product());
+  $('input[name="desc"]').val(faker.lorem.text());
+  $('input[name="manufacturer"]').val(faker.company.companyName());
+  $('input[name="owner"]').val(faker.company.companyName());
+});
