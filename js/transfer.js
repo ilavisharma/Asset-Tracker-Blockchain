928b7cb7e53c319ced2af32c0dbba0af7f01c772
ilavisharma <lavisharma796@gmail.com>
2019-03-01 19:34:24 +0530

    initial setup complete

--- js/transfer.js
+++ js/transfer.js
@@ -1,1 +1,45 @@
-console.log("connected");
+function searchAsset() {
+  // clear previous search result
+  $("#searchResult").html("");
+
+  //   now search the asset
+  assetId = parseInt($('input[name="id"]').val());
+  AssetTrackerContract.methods.getAsset(assetId).call((error, response) => {
+    if (error) console.log(error);
+    else {
+      // if found
+      if (response[1] !== "") {
+        let content =
+          "<h4>Asset Found</h4>Name: " +
+          response[1] +
+          "<br>" +
+          "Owner: " +
+          response[3] +
+          "<br>" +
+          "Current Status: " +
+          response[4];
+        $("#searchResult").append(content);
+        $("#transferFrom").show();
+      } else {
+        //   if not found
+        let content = "<h4>Asset Not Found</h4>";
+        $("#searchResult").append(content);
+        $("#transferFrom").hide();
+      }
+    }
+  });
+}
+
+function transferAsset() {
+  let assetId = parseInt($('input[name="id"]').val());
+  let newOwner = $('input[name="newOwner"]').val();
+  let newStatus = $('input[name="newStatus"]').val();
+
+  // transfer the asset
+  AssetTrackerContract.methods
+    .transferAsset(assetId, newOwner, newStatus)
+    .send()
+    .then(tx => {
+      console.log(tx);
+    });
+}
