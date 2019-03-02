fd82c7173d667b9a34d56d4841113119ba8d5d2c
ilavisharma <lavisharma796@gmail.com>
2019-03-02 09:52:27 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,45 @@ const AssetTrackerABI = [
     constant: false,
     inputs: [
       {
+        name: "_batchNo",
+        type: "string"
+      },
+      {
+        name: "_name",
+        type: "string"
+      },
+      {
+        name: "_description",
+        type: "string"
+      },
+      {
+        name: "_manufacturer",
+        type: "string"
+      },
+      {
+        name: "_owner",
+        type: "string"
+      },
+      {
+        name: "_status",
+        type: "string"
+      }
+    ],
+    name: "createAsset",
+    outputs: [
+      {
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
         name: "_id",
         type: "uint256"
       },
@@ -27,12 +66,90 @@ const AssetTrackerABI = [
     type: "function"
   },
   {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "id",
+        type: "uint256"
+      },
+      {
+        indexed: false,
+        name: "manufacturer",
+        type: "string"
+      },
+      {
+        indexed: false,
+        name: "status",
+        type: "string"
+      }
+    ],
+    name: "AssetCreate",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "id",
+        type: "uint256"
+      },
+      {
+        indexed: false,
+        name: "newOwner",
+        type: "string"
+      }
+    ],
+    name: "AssetTransfer",
+    type: "event"
+  },
+  {
     constant: true,
     inputs: [],
-    name: "getAssetCount",
+    name: "assetCount",
     outputs: [
       {
         name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "",
+        type: "uint256"
+      }
+    ],
+    name: "AssetStore",
+    outputs: [
+      {
+        name: "id",
+        type: "uint256"
+      },
+      {
+        name: "batchNo",
+        type: "string"
+      },
+      {
+        name: "name",
+        type: "string"
+      },
+      {
+        name: "description",
+        type: "string"
+      },
+      {
+        name: "manufacturer",
+        type: "string"
+      },
+      {
+        name: "statusCount",
         type: "uint256"
       }
     ],
@@ -82,7 +199,7 @@ const AssetTrackerABI = [
   {
     constant: true,
     inputs: [],
-    name: "assetCount",
+    name: "getAssetCount",
     outputs: [
       {
         name: "",
@@ -92,84 +209,6 @@ const AssetTrackerABI = [
     payable: false,
     stateMutability: "view",
     type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_batchNo",
-        type: "string"
-      },
-      {
-        name: "_name",
-        type: "string"
-      },
-      {
-        name: "_description",
-        type: "string"
-      },
-      {
-        name: "_manufacturer",
-        type: "string"
-      },
-      {
-        name: "_owner",
-        type: "string"
-      },
-      {
-        name: "_status",
-        type: "string"
-      }
-    ],
-    name: "createAsset",
-    outputs: [
-      {
-        name: "",
-        type: "uint256"
-      }
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        name: "id",
-        type: "uint256"
-      },
-      {
-        indexed: false,
-        name: "manufacturer",
-        type: "string"
-      },
-      {
-        indexed: false,
-        name: "status",
-        type: "string"
-      }
-    ],
-    name: "AssetCreate",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        name: "id",
-        type: "uint256"
-      },
-      {
-        indexed: false,
-        name: "newOwner",
-        type: "string"
-      }
-    ],
-    name: "AssetTransfer",
-    type: "event"
   }
 ];
 
@@ -181,7 +220,7 @@ if (typeof web3 !== "undefined") {
 
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
-const contractAddress = "0xe35622a154075704e9de579f09f1f10b004e14dc";
+const contractAddress = "0x98c75b1ed8066c6834872b272018144006e14ab0";
 var fromAddress = "0x7F0D09B5514D2e79271EfD258e1A732Ca5EE89Bf";
 
 const AssetTrackerContract = new web3.eth.Contract(
