6804d791e51f3fe8492ef7d1cd649e45be62e3db
ilavisharma <lavisharma796@gmail.com>
2019-03-02 19:52:59 +0530

    display path of the asset

--- js/searchAsset.js
+++ js/searchAsset.js
@@ -1,6 +1,6 @@
 function searchAsset() {
   $("#loading").show();
-  var id = parseInt($('input[name="id"]').val());
+  let id = parseInt($('input[name="id"]').val());
 
   // search in the smart contract
   AssetTrackerContract.methods.getAsset(id).call((error, response) => {
@@ -35,12 +35,42 @@ function searchAsset() {
 
         // show the status history
         $("#statusHistory").show();
+        // asset history
+        assetHistory(id);
       } else {
         // asset is not found
         let result = "<h3>Asset Not Fuund</h3>";
         $("#searchResult").html("");
         $("#loading").hide();
         $("#searchResult").append(result);
+      }
+    }
+  });
+}
+
+function assetHistory(id) {
+  AssetTrackerContract.methods.AssetStore(id).call((error, response) => {
+    if (error) console.log(error);
+    else {
+      let statusCount = parseInt(response[5]);
+      for (let i = statusCount; i >= 1; i--) {
+        AssetTrackerContract.methods
+          .getStatus(id, i)
+          .call((error, response) => {
+            if (error) console.log(error);
+            else {
+              let date = new Date(parseInt(response[0]) * 1000);
+              let event =
+                date +
+                "<br>" +
+                response[2] +
+                "<br>" +
+                response[1] +
+                "<br>" +
+                "<br>";
+              $("#statusHistory").append(event);
+            }
+          });
       }
     }
   });
